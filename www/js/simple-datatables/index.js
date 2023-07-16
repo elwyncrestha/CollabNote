e8aa79e01f68020f99d4fd4afea00792d3c9f035
Elvin Shrestha <elwyncrestha@gmail.com>
2023-07-16 12:27:37 +1000

    Fix Simple DataTable layout issue.

--- www/js/simple-datatables/index.js
+++ www/js/simple-datatables/index.js
@@ -8,4 +8,19 @@ window.addEventListener('DOMContentLoaded', () => {
     if (datatablesSimple) {
         new DataTable(datatablesSimple);
     }
+    
+    // SB Admin template CSS had very tight coupling with Bootstrap CSS.
+    // Had to remove Bootstrap CSS from SB Admin template CSS.
+    // And, add a Bootstrap (from `package.json`) class to Simple DataTables elements.
+    const elementList = [
+        { selector: '.datatable-selector', classList: ['form-select'] },
+        { selector: '.datatable-input', classList: ['form-control'] },
+        { selector: '.datatable-wrapper .datatable-container', classList: ['table-responsive'] },
+        { selector: '.datatable-table', classList: ['table'] },
+    ];
+    elementList.forEach(({ selector, classList }) => {
+        const datatableSelectors = document.querySelectorAll(selector);
+        datatableSelectors.forEach((selector) => selector.classList.add(...classList));    
+    })
+    
 });
